6bc67653a077618ca831a44f58ac03000f5066c6
Johannes Higgins <44159791+Morllum@users.noreply.github.com>
2022-12-13 11:13:10 +0000

    this was a 5kyu actually

--- 6kyu.js
+++ 6kyu.js
@@ -1,12 +1,24 @@
-function solution(number) {
-    let total = 0;
-    for (i = 1; i < number; i++) {
-        if (i % 3 === 0) {
-            total += i;
-        } else if (i % 5 === 0) {
-            total += i;
-        }
-    } return total
-}
+// function solution(number) {
+//     let total = 0;
+//     for (i = 1; i < number; i++) {
+//         if (i % 3 === 0) {
+//             total += i;
+//         } else if (i % 5 === 0) {
+//             total += i;
+//         }
+//     } return total
+// }
 
-console.log(solution(10))
+// console.log(solution(10))
+
+// function moveZeros(arr) {
+//     for (let each of arr) {
+//         if (each === 0) {
+//             arr.splice(arr.indexOf(0), 1);
+//             arr.push(0);
+//         }
+//     } return arr
+// }
+
+// console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]))
+
