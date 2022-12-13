add5dc75b821f18fda7cd46c0297958f398c5bac
Morllum <johanhiggins@gmail.com>
2022-12-13 22:28:53 +0000

    final one for the day

--- 6kyu.js
+++ 6kyu.js
@@ -22,3 +22,16 @@
 
 // console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]))
 
+function pigIt(str) {
+    let newStr = ""
+    let splitString = str.split(" ");
+    var regExp = /[a-zA-Z]/;
+    console.log(splitString)
+    for (let word of splitString) {
+        if (regExp.test(word)) {
+            newStr += word.slice(1) + word[0] + "ay" + " ";
+        } else { newStr += word }
+    } return newStr.trimEnd()
+}
+
+console.log(pigIt('Pig latin is cool')); 
